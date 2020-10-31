479557c8bdfb1ca0efc7d7e03cecd9779a1f5c5c
Shreyam Saha <sahashreyam123@gmail.com>
2020-10-31 22:07:09 +0530

    Update Shop.java

--- Shop.java
+++ Shop.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Shop{
     public static void main(String[] args) {
-        String[] softToys ={"Giant-Teddy-Bear","Giraffe","Cat","Mega-Bear","Dog","Lion","Billy-Bear","Besty-Bear","Monkey","Bobby-Bear","Bunny-Rabbit","Benjamin-Bear","Kung-Fu-Panda","Brown-Bear","Pink-Bear","Baby-Elephant","Blue-Fish","Hippo","Cute-Pig","Pikachu","Doremon","Tortoise","Cater-Pillar","Candy-Doll"};
+        String[] softToys ={"Giant-Teddy-Bear","Giraffe","Cat","Mega-Bear","T-rex","Big-Soft-Eagle","Dog","Lion","Billy-Bear","Besty-Bear","Monkey","Bobby-Bear","Bunny-Rabbit","Benjamin-Bear","Kung-Fu-Panda","Brown-Bear","Pink-Bear","Baby-Elephant","Blue-Fish","Hippo","Cute-Pig","Pikachu","Doremon","Tortoise","Cater-Pillar","Candy-Doll"};
         Scanner sc = new Scanner(System.in);
 
         String item = sc.nextLine();
